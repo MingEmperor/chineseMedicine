83be3750d8addf43ff57194d409ed3d909465d06
wangzhenyu <wangzhenyu01@meicai.cn>
2019-04-10 14:03:07 +0800

    fix: 查看处方页面完成

--- src/components/prescriptionManage/prescriptions.vue
+++ src/components/prescriptionManage/prescriptions.vue
@@ -1,8 +1,87 @@
 <template>
   <div class="container">
+    <!-- 标题、日期、所用药材、备注、图片 -->
     <div class="toolbar-index">
+      <div class="toolbar-back">
+        ➡️
+      </div>
       <input class="toolbar-input" type="text" placeholder="这里输入想查询的处方.."/>
       <div class="toolbar-btn">搜索</div>
+    </div>
+    <div class="presriptions-warpper">
+      <div  class="presriptions-item">
+        <div class="presriptions-title">
+          <span class="presriptions-title-text">
+            七月七日诊侯智川先生
+          </span>
+          <span class="presriptions-title-date">
+            2018-07-07
+          </span>
+        </div>
+        <div class="presriptions-use">
+          <p class="qwe">
+            三七(33g)
+          </p>
+          <p class="qwe">
+            麻黄
+          </p>
+           <p class="qwe">
+            灵芝
+          </p>
+          <p class="qwe">
+            人参
+          </p>
+           <p class="qwe">
+            五谷
+          </p>
+        </div>
+        <div class="presriptions-desc">
+          备注
+        </div>
+        <div class="presriptions-img">
+          <img class="" src="../../assets/箭头.png" />
+        </div>
+      </div>
+    </div>
+
+    <div class="presriptions-warpper">
+      <div  class="presriptions-item">
+        <div class="presriptions-title">
+          <span class="presriptions-title-text">
+            七月七日诊侯智川先生
+          </span>
+          <span class="presriptions-title-date">
+            2018-07-07
+          </span>
+        </div>
+        <div class="presriptions-use">
+          <p class="qwe">
+            三七(33g)
+          </p>
+          <p class="qwe">
+            麻黄
+          </p>
+           <p class="qwe">
+            灵芝
+          </p>
+          <p class="qwe">
+            人参
+          </p>
+           <p class="qwe">
+            五谷
+          </p>
+        </div>
+        <div class="presriptions-desc">
+          备注备注备注备注备注备注备注
+        </div>
+        <div class="presriptions-img">
+          <img class="" src="../../assets/箭头.png" />
+        </div>
+      </div>
+    </div>
+
+    <div class="float-add">
+      +
     </div>
   </div>
 </template>
@@ -12,37 +91,15 @@ import purchasingRecord from '../common/purchasingRecords'
 export default {
   data () {
     return {
-      IsSelect: true,
-      swiperOption: {
-        autoplay: 4000,
-        initialSlide: 1,
-        pagination: '.swiper-pagination',
-        loop: true
-      },
-      func: [{
-        id: 1,
-        imgUrl: require('../../assets/处方.png'),
-        desc: '查看处方'
-      }, {
-        id: 2,
-        imgUrl: require('../../assets/中药.png'),
-        desc: '三旬药分'
-      }, {
-        id: 3,
-        imgUrl: require('../../assets/药材.png'),
-        desc: '订购药材'
-      }, {
-        id: 4,
-        imgUrl: require('../../assets/动态.png'),
-        desc: '医术交流'
-      }],
-      imgList: []
     }
   },
   computed: {
     showSwiper () {
       return this.imgList.length
     }
+  },
+  methods: {
+
   },
   components: {
     purchasingRecord
@@ -51,7 +108,7 @@ export default {
 </script>
 
 <style scoped>
-.toolbar-index{
+.toolbar-index {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -59,16 +116,7 @@ export default {
   height: 3.4rem;
   background-color: #D9D7C1;
 }
-.toolbar-input{
-  padding-left: .5rem;
-  width: 65%;
-  height: 2.8rem;
-  line-height: 2.8rem;
-  margin-left: 3.8rem;
-  font-size: 1.4rem;
-  border-radius: .35rem;
-}
-.toolbar-btn{
+.toolbar-back {
   width: 4rem;
   height: 2.8rem;
   line-height: 2.8rem;
@@ -77,55 +125,91 @@ export default {
   font-size: 1.4rem;
   color:#ffffff;
 }
-.func-warpper{
-  display: flex;
-  justify-content: space-between;
-  margin-top: 1rem;
-  width: 100%;
-  height: 5rem;
-}
-.func-item-warpper{
-  display: inline-block;
-  position: relative;
-  width: 23%;
-  height: 100%;
-}
-.func-item-img{
-  position: absolute;
-  left: 50%;
-  margin-top: .3rem;
-  margin-left: -1.4rem;
-  width: 2.8rem;
+.toolbar-input {
+  padding-left: .5rem;
+  width: 65%;
   height: 2.8rem;
+  line-height: 2.8rem;
+  margin-left: .5rem;
+  font-size: 1.4rem;
+  border-radius: .35rem;
 }
-.func-item-desc{
-  position: absolute;
-  left: 50%;
-  margin-top: 3.5rem;
-  margin-left: -2.2rem;
-  width: 4.4rem;
-  height: 1.2rem;
-  line-height: 1.2rem;
+.toolbar-btn {
+  width: 4rem;
+  height: 2.8rem;
+  line-height: 2.8rem;
+  margin-left: 1rem;
   text-align: center;
-  color: #000000;
-  font-size: .9rem;
+  font-size: 1.4rem;
+  color:#ffffff;
 }
-.myswiper >>> .swiper-pagination-bullet-active {
-    background:#fff !important;
+.presriptions-warpper{
+  position: relative;
+  width: 96%;
+  margin: 0 auto;
+  margin-top: 2rem;
+  border: 1px dashed #909399;
 }
-.swiper-wrapper{
-  position:relative;
+.presriptions-title{
+  position: absolute;
+  display: flex;
+  justify-content: space-around;
+  top: -1.5rem;
+  left: 2rem;
+  height: 3rem;
+  line-height: 3rem;
+  background-color: #ffffff;
+}
+.presriptions-title-text{
+  font-size: 1.4rem;
+  font-weight: bold;
+  margin-right: 2rem;
+}
+.presriptions-use{
+  width: 96%;
+  margin: 0 auto;
+  min-height: 6rem;
+  padding-top:1.8rem;
+  border-bottom: 1px dashed #e3e3e3;
+}
+.qwe{
+  display: inline-block;
+  min-width: 6.6rem;
+  margin: .5rem;
+  padding: 0 .6rem;
+  height: 2.4rem;
+  line-height: 2.4rem;
+  text-align: center;
+  font-size: 1.3rem;
+  font-family: 'Gill Sans';
+  border-radius: .35rem;
+  color: #909399;
+  background-color: rgba(144,147,153,.1);
+  border: 1px solid rgba(144,147,153,.2);
+}
+.presriptions-desc{
   width: 100%;
-  height: 0;
-  overflow: hidden;
-  padding-bottom: 37%;
-  background: #eee;
+  height:3rem;
+  line-height:3rem;
+  padding-left: .5rem;
+  color: #909399;
 }
-.swiper-img {
-    width: 100%;
+.presriptions-img{
+  margin: .8rem auto;
+  width: 96%;
 }
-.swiper-pagination {
-   position:absolute;
-   top:60%;
+.float-add{
+  z-index: 999;
+  position: sticky;
+  bottom: 1rem;
+  margin: 0 auto;
+  width: 4rem;
+  height: 4rem;
+  line-height: 4rem;
+  text-align: center;
+  border-radius: 2rem;
+  color: #ffffff;
+  font-size: 2.3rem;
+  background-color: #D9D7C1;
 }
 </style>
